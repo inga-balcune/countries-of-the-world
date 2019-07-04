50b4971f6f8671c591983c90b2dc72127438f6ac
Inga Balcune <inga.balcune@gmail.com>
2019-07-04 15:25:29 +0200

    error page button added

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,13 @@
 <template>
   <div class='error-section' :style="{ 'background-image': 'linear-gradient(rgba(234, 238, 240,0.9), rgba(234, 238, 240,0.9)), url(' + image + ')' }">
     
-    <h2 class='error-section__message'>Oops, an error!</h2>
+    <div class='error-section__container'>
+      <h2 class='error-section__container__message'>Oops, an error!</h2>
+      <a class='page-button' href='/'>Back to homepage</a>
+    </div>
     
   </div>
 </template>
-
 <script>
 export default {
    data() {
@@ -25,10 +27,17 @@ export default {
   background-size: cover;
   background-position: center;
 
-  &__message {
+  &__container {
     @include center-elem;
-    margin-top: -10rem;
+    text-align: center;
+
+    &__message {
+      margin-top: -10rem;
+      margin-bottom: 1rem;
+    }
   }
+  
 }
+
 
 </style>
